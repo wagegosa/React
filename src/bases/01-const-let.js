0f48bcf1893b8c8d99c063e2ebd61712fc957a58
Gerardo González <wagegosa@gmail.com>
2021-02-20 14:11:47 -0500

    clase de constantes.

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,15 @@
+// varibles y constantes
+
+const name = "Walter";
+const lastName = "González";
+
+// let significa qie es variable
+let valorDado = 5;
+
+console.log( name + " " + lastName + "valor dado"+ valorDado)
+
+if (true) {
+    const name = "Laura";
+    console.log(name)
+}
+console.log(name)
